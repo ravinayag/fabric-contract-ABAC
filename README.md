e33dd0ba109bd0355c60408c5e3149017ce32941
ravinayag <ravinayag@gmail.com>
2020-07-03 12:57:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Read ME First
+
+This is the Fork of [IBM_FCA_BAC](https://github.com/IBM/fabric-contract-attribute-based-access-control) to deploy without vsc and IBM cloud platform.
+
+My fork is useful, if you deploy only the fabric network part with 1 org, 2 peers, orderer & CA,  The rest of the actions are followed from the IBM FCA-BAC procedure.
+
+## Start the Fabric. 
+
+* 1, Clone this repo under ```/opt/bc/fcabac```  folder 
+* 2, Copy fabric-samples-explained to fabric-samples/ folder. 
+* 3, Move your working directory location to ```~/fabric-samples/fabric-samples-explained/```
+* 4, To start the fabric network, Run the script 
+    ```./test-start.sh```
+* 5, Once the Network up,  You have to install, and instantiate the Chaincode.
+``` ./gensuply-install-cc.sh ```
+ Once you have sucessfully Instantiateed the chaincode. you have to start the server and client side application services.
+ * 6, Start the server service  from ```/opt/bc/fcabac/application/server/```
+  ``` 
+  npm install 
+  npm install fabric-ca-client@1.4.0            <<<< incase if you face error moudle is not found.
+  node server & 
+  ```
+ * 7, Once the service is up, you move to client folder /opt/bc/fcabac/application/client
+  ``` 
+  npm install
+  ng serve &
+  ```
+ * 8, Thats all, Your fabric is up with IBM,'s FCABAC is available for your learning service 
+
+Thanks
+
---
+++ gensuply-install-cc.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+###Installcode
+
+export chainname=gensupplychainnet      #gensupply
+export signpackge=gensupplychainnet     #gensupply
+docker exec cli peer chaincode list --installed
+docker exec cli peer chaincode list --instantiated -C mychannel
+
+### Non sign package install
+docker exec cli peer chaincode package $chainname.out -n $chainname -v 1.0 -p /opt/gopath/src/github.com/chaincode/fcabac/contract -l node  # -s -S -i "AND('OrgA.admin')" 
+docker exec cli peer chaincode install $chainname.out -n $chainname -v 1.0 -l node
+#or
+#docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" 
+#            -e "CORE_PEER_ID=peer1.org1.example.com" 
+#            -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" 
+#            -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" 
+#            -e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key" 
+#            -e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt" 
+#            -e "CORE_PEER_TLS_ENABLED=true" 
+#            -e "CORE_PEER_ADDRESS=peer1.org1.example.com:8051" cli peer chaincode install  $chainname.out -n $chainname -v 1.0 -l node
+
+
+
+### Signed package
+#docker exec cli peer chaincode package $chainname.out -n $chainname -v 1.0 -p /opt/gopath/src/github.com/chaincode/fcabac/contract -l node -s -S -i "AND('OrgA.admin')" 
+#docker exec cli peer chaincode signpackage $chainname.out $signpackge.out
+#docker exec cli peer chaincode install $signpackge.out -n $chainname -v 1.0 -l node
+#or
+#docker exec -e  "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" 
+#            -e "CORE_PEER_ID=peer1.org1.example.com" 
+#            -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" 
+#            -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" 
+#            -e "CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key" 
+#            -e "CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt" 
+#            -e "CORE_PEER_TLS_ENABLED=true" 
+#            -e "CORE_PEER_ADDRESS=peer1.org1.example.com:8051" cli peer chaincode install  $signpackge.out -n $chainname -v 1.0 -l node
+
+
+export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+docker exec cli peer chaincode instantiate -o orderer.example.com:7050 --tls true --cafile $ORDERER_CA -C mychannel $chainname.out -n $chainname -v 1.0 -l node -c '{"Args":["init"]}'  -P "OR ('Org1MSP.member' , 'Org1MSP.peer')"
+### signpackage
+#docker exec cli peer chaincode instantiate -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel $signpackge.out -n $chainname -v 1.0 -l node -c '{"Args":["init"]}'  -P "OR ('Org1MSP.member' , 'Org1MSP.peer')"
+
+
+rm -rf /opt/bc/fcabac/gateway/local/gen_local_wallet 
+
+echo " sleeping 15 secs"
+sleep 15 
+
+####  for FCABAC Project only
+#export chainname1=ledgerapi
+
+### Non sign package install
+#docker exec cli peer chaincode package $chainname1.out -n $chainname1 -v 1.0 -p /opt/gopath/src/github.com/chaincode/fcabac/contract/ledger-api -l node  # -s -S -i "AND('OrgA.admin')"
+#docker exec cli peer chaincode install $chainname1.out -n $chainname1 -v 1.0 -l node
+
+
+#export chainname2=ordercontract
+
+### Non sign package install
+#docker exec cli peer chaincode package $chainname2.out -n $chainname2 -v 1.0 -p /opt/gopath/src/github.com/chaincode/fcabac/contract/lib/ -l node  # -s -S -i "AND('OrgA.admin')"
+#docker exec cli peer chaincode install $chainname2.out -n $chainname2 -v 1.0 -l node
+
+
+
+cd /opt/bc/fcabac
+
