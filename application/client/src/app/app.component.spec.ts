577308dd19765909a7928337f83dfddf0d9ee3e7
ravi <ravindran@ksmail.tech>
2020-08-05 23:50:52 +0530

    barcode add -client

--- application/client/src/app/app.component.spec.ts
+++ application/client/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to generic supplychain!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to generic supplychain Logistics!');
   });
 });
